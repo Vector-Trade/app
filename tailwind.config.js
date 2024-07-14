36347021cfe545ef0dfee6678288a619a15fb266
Rizwan <rizwan.m.1610@gmail.com>
2024-07-14 14:54:26 +0530

    Second draft

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+      extend: {
+        fontSize: {
+          '10xl': '10rem',
+          '11xl': '12rem',
+          // Add more sizes as needed
+        }
+      }
+    }
+  }
